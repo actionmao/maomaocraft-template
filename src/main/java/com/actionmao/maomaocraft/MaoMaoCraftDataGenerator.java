914de7b448b916a851c726129aa318a1de2ccef3
ActionMao <actionmao1@gmail.com>
2025-08-06 12:02:41 +0800

    Change fabric.mod.json

---
+++ src/main/java/com/actionmao/maomaocraft/MaoMaoCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.actionmao.maomaocraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MaoMaoCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
