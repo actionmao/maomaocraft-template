812b113f9b89e47b6e00daebf02468ed4d5c5576
ActionMao <actionmao1@gmail.com>
2025-10-04 11:31:37 +0800

    Fix some bugs of class 'Miner'

--- src/main/java/com/actionmao/maomaocraft/item/custom/Miner.java
+++ src/main/java/com/actionmao/maomaocraft/item/custom/Miner.java
@@ -103,7 +103,7 @@ public class Miner extends Item {
                                 newList.addAll(prospect);
                             }
                         } else {
-                            break;
+                            return newList;
                         }
 
 
